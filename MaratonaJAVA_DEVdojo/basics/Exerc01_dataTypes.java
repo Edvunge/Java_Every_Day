0731c20cb74536fe7c1af58a09c544c2b299231a
edvunge <costaedy18@gmail.com>
2024-01-25 01:35:16 +0000

    day03 - dataTypes

---
+++ MaratonaJAVA_DEVdojo/basics/Exerc01_dataTypes.java
@@ -0,0 +1,11 @@
+package MaratonaJAVA_DEVdojo.basics;
+
+public class Exerc01_dataTypes {
+    public static void main(String[] args) {
+        String nome = "edvaldo";
+        String endereco = "rua almada negreiros";
+        int telefone = 932605657;
+
+        System.out.printf("O %s domiciliado no endereco %s \ne telefone %d nao possui nenhum tipo de pendencia",nome ,endereco ,telefone);
+    }
+}
