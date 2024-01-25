35cf36d157cde476161fedcd3489e6d7b6650716
edvunge <costaedy18@gmail.com>
2024-01-25 23:50:11 +0000

    day03 - if_and_else

---
+++ MaratonaJAVA_DEVdojo/basics/ControleFluxo1.java
@@ -0,0 +1,13 @@
+package MaratonaJAVA_DEVdojo.basics;
+
+public class ControleFluxo1 {
+    public static void main(String[] args) {
+        int age = 28;
+
+        if (age >= 18) {
+            System.out.println("voce eh maior de idade: ");
+        } else {
+            System.out.println("voce nao eh maior de idade: ");
+        }
+    }
+}
