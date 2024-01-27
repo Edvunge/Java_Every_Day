5b7d043e3afba254b845eb31d5c2b87a54cdc440
edvunge <costaedy18@gmail.com>
2024-01-27 10:16:17 +0000

    day05 - Strings intro

---
+++ JavaNotesForProfessionals/cap11_Strings/Localizando_Cadeia_Caracteres.java
@@ -0,0 +1,24 @@
+package JavaNotesForProfessionals.cap11_Strings;
+
+public class Localizando_Cadeia_Caracteres {
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        String str2 = "Hello";
+        String str3 = "helLO";
+
+        System.out.println(str1.contains(str2)); //prints true
+        System.out.println(str1.contains(str3)); //prints false
+
+        String s = "this is a long sentence";
+        int i = s.indexOf('i');
+        int j = s.indexOf("long");
+        int k = s.indexOf('z');
+        int h = s.indexOf("LoNG");
+
+        str1 = "Hello World";
+        str2 = "w0r";
+        str1.indexOf(str2);
+        str1.toLowerCase().contains(str2.toLowerCase());
+        str1.toLowerCase().indexOf(str2.toLowerCase());
+    }
+}
