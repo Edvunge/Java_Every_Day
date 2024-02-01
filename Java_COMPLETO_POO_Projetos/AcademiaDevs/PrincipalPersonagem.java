4d49127a59c897e4292fc37b4c2376a289c79a21
edvunge <costaedy18@gmail.com>
2024-02-01 23:53:55 +0000

    day9 - methods

---
+++ Java_COMPLETO_POO_Projetos/AcademiaDevs/PrincipalPersonagem.java
@@ -0,0 +1,13 @@
+package Java_COMPLETO_POO_Projetos.AcademiaDevs;
+
+public class PrincipalPersonagem {
+    public static void main(String[] args) {
+        Personagem heroi = new Personagem();
+        heroi.nome = "Hercules";
+        heroi.nivel = 2;
+        heroi.forca = 16;
+
+        heroi.mostraStatus();
+        heroi.atacar("Hydra", "");
+    }
+}
