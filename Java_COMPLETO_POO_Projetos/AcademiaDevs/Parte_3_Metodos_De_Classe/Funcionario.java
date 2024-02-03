5b6c1e0e782414398197a5c3ed975f6c8264dd78
edvunge <costaedy18@gmail.com>
2024-02-03 00:29:19 +0000

    Day11 - Metodos_de_classe

---
+++ Java_COMPLETO_POO_Projetos/AcademiaDevs/Parte_3_Metodos_De_Classe/Funcionario.java
@@ -0,0 +1,11 @@
+package Java_COMPLETO_POO_Projetos.AcademiaDevs.Parte_3_Metodos_De_Classe;
+
+public class Funcionario {
+    String nome;
+    double salario;
+    double valeRefeicaoDiario;
+
+    public  void aumentaSalario(double aumento) {
+        this.salario += aumento;
+    }
+}
