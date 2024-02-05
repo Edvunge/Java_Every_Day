e167b749b710e6bc8b692a85c8f30a42a0e8f707
edvunge <costaedy18@gmail.com>
2024-02-05 22:28:04 +0000

    Day13 - exercises

---
+++ News_Staffs/Dialog1.java
@@ -0,0 +1,9 @@
+package News_Staffs;
+
+import javax.swing.*;
+
+public class Dialog1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to java");
+    }
+}
