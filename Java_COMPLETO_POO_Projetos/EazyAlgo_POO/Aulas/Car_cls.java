73a23f6f2697c71a0dddc6c433c69e6a4e2d9c9f
edvunge <costaedy18@gmail.com>
2024-02-10 15:29:52 +0000

    Day19 - new course tutorial

---
+++ Java_COMPLETO_POO_Projetos/EazyAlgo_POO/Aulas/Car_cls.java
@@ -0,0 +1,8 @@
+package Java_COMPLETO_POO_Projetos.EazyAlgo_POO.Aulas;
+
+public class Car_cls {
+    private int doors;
+    private int gears;
+    public String ModelName;
+    private String Color;
+}
---
+++ Java_COMPLETO_POO_Projetos/EazyAlgo_POO/Aulas/Getters_and_Setters_aula_2.java
@@ -0,0 +1,13 @@
+package Java_COMPLETO_POO_Projetos.EazyAlgo_POO.Aulas;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class Getters_and_Setters_aula_2 {
+    public static void main(String[] args) {
+        Cars audi = new Cars(2, 6, "R8", "Black");
+        System.out.println(audi.getColor());
+        System.out.println(audi.getGears());
+        System.out.println(audi.getModelName());
+        System.out.println(audi.getDoors());
+    }
+}
