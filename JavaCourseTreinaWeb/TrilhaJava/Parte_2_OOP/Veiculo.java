629d066a537d4a2d7de9a46948e8ada23333999f
edvunge <costaedy18@gmail.com>
2024-02-11 20:58:19 +0000

    Day20 - exercs

---
+++ JavaCourseTreinaWeb/TrilhaJava/Parte_2_OOP/Veiculo.java
@@ -0,0 +1,18 @@
+package JavaCourseTreinaWeb.TrilhaJava.Parte_2_OOP;
+
+public class Veiculo {
+    public String nome;
+    public String marca;
+    public String chassis;
+    public int quantidadesRodas;
+
+    void ligar(){
+
+    }
+
+    void desligar(){
+
+    }
+
+   // float abastecer(float )
+}
