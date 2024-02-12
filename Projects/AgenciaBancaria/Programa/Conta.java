12792b5cd4cfad19372f2c525fdfb8ab050ace61
edvunge <costaedy18@gmail.com>
2024-02-12 01:40:56 +0000

    Day21 - agenciaBancaria project

---
+++ Projects/AgenciaBancaria/Programa/Conta.java
@@ -0,0 +1,51 @@
+package Projects.AgenciaBancaria.Programa;
+
+import Projects.AgenciaBancaria.Utilitarios.Utils;
+
+public class Conta {
+
+    private static int contadorDeContas = 1;
+
+    private int numeroConta;
+    private Pessoa pessoa;
+    private Double saldo = 0.0;
+
+    public Conta(int numeroConta, Pessoa pessoa, Double saldo) {
+        this.numeroConta = contadorDeContas;
+        this.pessoa = pessoa;
+        contadorDeContas += 1;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String toString() {
+        return "\nNumero da conta: " + this.getNumeroConta() +
+                "\nNome: " + this.pessoa.getNome() +
+                "\nCPF: " + this.pessoa.getCpf() +
+                "\nEmail: " + this.pessoa.getEmail() +
+                "\nSaldo: " + Utils.doubleToString(this.getSaldo()) +
+                "\n";
+    }
+}
