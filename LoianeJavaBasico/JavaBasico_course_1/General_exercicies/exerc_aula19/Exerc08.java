4ed65f1f2b804df5be011bfdbfb0941eb782a8ab
edvunge <costaedy18@gmail.com>
2024-02-13 02:32:10 +0000

    Day22 - exercises vetores

--- LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula19/Exerc08.java
+++ LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula19/Exerc08.java
@@ -1,19 +1,20 @@
 package LoianeJavaBasico.JavaBasico_course_1.General_exercicies.exerc_aula19;
 
-public class Exerc07 {
+public class Exerc08 {
     public static void main(String[] args) {
         /*
-            7. Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
-            um vetor C, onde cada elemento de C é a subtração dos respectivos
-            elementos em A e B, ou seja: C[i] = A[i] – B[i].
+        8. Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
+        um vetor C, onde cada elemento de C é a multiplicação dos
+        respectivos elementos em A e B, ou seja: C[i] = A[i] * B[i].
         */
+
         int len = 10;
         int vetA[] = {34, 32, 38, 36, 32, 40, 21, 62, 41, 21};
         int vetB[] = {42, 22, 28, 26, 10, 14, 12, 16, 14, 12};
         int vetC[] = new int[10];
 
         for (int j = 0; j < len; j++) {
-            vetC[j] = vetA[j] - vetB[j];
+            vetC[j] = vetA[j] * vetB[j];
         }
 
         System.out.println("\n");
---
+++ LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula19/Exerc15.java
@@ -0,0 +1,27 @@
+package LoianeJavaBasico.JavaBasico_course_1.General_exercicies.exerc_aula19;
+
+public class Exerc15 {
+    public static void main(String[] args) {
+        /*
+            15. Criar um vetor A com 10 elementos inteiros. Desenvolver um programa
+            que defina o percentual de elementos pares e ímpares,
+            respectivamente, armazenados neste vetor.
+        */
+
+        int len = 10;
+        int vetA[] = {34, 32, 38, 36, 32, 40, 21, 62, 41, 21};
+        int soma = 0;
+
+        // calculo de percentagem
+        /*
+        salario = (valor * desconto / 100)
+        mult = *
+        qutddPar = (  / 100)
+        */
+
+        // calculo de pares e impares
+
+
+        // valorFinal
+    }
+}
