a94c2afa843e3dfb99dd2febbda06c0c1038cf72
edvunge <costaedy18@gmail.com>
2024-02-15 23:54:46 +0000

    Day24 - newProject_SimplesTetris

---
+++ Projects/SimpleTetris/Main/PlayManager.java
@@ -0,0 +1,21 @@
+package Projects.SimpleTetris.Main;
+
+public class PlayManager {
+    // Main Play Area
+    final int WIDTH = 360;
+    final int HEIGHT = 600;
+    public static int left_x;
+    public static int right_x;
+    public static int top_y;
+    public static int bottom_y;
+
+
+    public PlayManager() {
+
+        // Main Play Area Frame
+        left_x = (GamePanel.WIDTH/2) - (WIDTH/2);
+        right_x = left_x + WIDTH;
+        top_y = 50;
+        bottom_y = top_y + HEIGHT;
+    }
+}
