bcc9ddf3ff8298c37524544e3d14687d102171cf
edvunge <costaedy18@gmail.com>
2024-02-18 23:06:08 +0000

    Day27 - update exercs

--- LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula19/ex23_a_ex42/Exerc23.java
+++ LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula19/ex23_a_ex42/Exerc23.java
@@ -1,4 +1,4 @@
-package LoianeJavaBasico.JavaBasico_course_1.General_exercicies.exerc_aula19;
+package LoianeJavaBasico.JavaBasico_course_1.General_exercicies.exerc_aula19.ex23_a_ex42;
 
 public class Exerc23 {
     /*
@@ -8,53 +8,17 @@ public class Exerc23 {
         percorrer os elementos do vetor deve ser encerrado, como sugestão:
         utilize uma variável do tipo flag para atingir este propósito.
     */
-
-    /*
-    24.Números palíndromos são aqueles que escritos da direita para a
-esquerda têm o mesmo valor quando escritos da esquerda para a
-direita. Exemplo: 545; 789987; 97379; 123454321; etc. Escreva um
-programa que verifique se um dado vetor A de 10 elementos inteiros é
-um palíndromo, ou seja, se o primeiro elemento do vetor e igual ao
-último, se o segundo elemento do vetor é igual ao penúltimo e assim
-por diante até verificar todos os elementos ou chegar a conclusão que
-o vetor não é um palíndromo.
-     */
-
-
-    /*
-25.Criar um vetor A com 10 elementos inteiros. Construir um vetor B de
-mesmo tipo e tamanho, obedecendo as seguintes regras de formação:
-a) Bi deverá receber 1 quando Ai for par; b) Bi deverá receber 0
-quando Ai for ímpar.
-     */
-
-
-    /*
-26.Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
-um vetor C de mesmo tipo e tamanho, obedecendo as seguintes
-regras de formação: a) Ci deverá receber 1 quando Ai for maior que Bi;
-b) Ci deverá receber 0 quando Ai for igual a Bi; c) Ci deverá receber -1
-quando Ai for menor que Bi.
-     */
-
-    /*
-27.Criar um vetor A com 10 elementos inteiros. Construir um vetor B de
-mesmo tipo e tamanho, obedecendo as seguintes regras de formação:
-a) Bi deverá receber 'a' quando Ai for menor que 7; b) Bi deverá
-receber 'b' quando Ai for igual a 7; c) Bi deverá receber 'c' quando Ai for
-maior que 7 e menor que 10; d) Bi deverá receber 'd' quando Ai for
-igual a 10; e e) Bi deverá receber 'e' quando Ai for maior que 10.
-Sugestão: char B[10];
-     */
-
-    /*
-28.Ler um vetor A com 10 elementos e construir um vetor B de mesmo
-tipo e tamanho e com os mesmos elementos de A, sendo que estes
-deverão estar invertidos, ou seja, o primeiro elemento de A passa a
-ser o último de B, o segundo elemento de A passa a ser o penúltimo
-de B e assim por diante.
-     */
     public static void main(String[] args) {
+        int lengthARRAY = 10;
+        int vetA[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int numPares = 0;
 
+        for (int i = 0; i < lengthARRAY; i++) {
+            if(vetA[i] % 2 == 0) {
+                numPares++;
+            } else {
+                break;
+            }
+        }
     }
 }
