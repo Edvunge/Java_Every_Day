331ed136e63fc358010bfdd9293c87b6ab13b098
edvunge <costaedy18@gmail.com>
2024-02-25 21:52:37 +0000

    day33 - exercs

---
+++ Java_COMPLETO_POO_Projetos/AcademiaDevs/Parte_12_ArrayList/AppArrayList.java
@@ -0,0 +1,16 @@
+package Java_COMPLETO_POO_Projetos.AcademiaDevs.Parte_12_ArrayList;
+
+import java.util.ArrayList;
+
+public class AppArrayList {
+    public static void main(String[] args) {
+        String[] arrayEstatico = new String[5];
+        arrayEstatico[0] = "Goku";
+        arrayEstatico[1] = "Maria";
+        arrayEstatico[2] = "Pedro";
+        arrayEstatico[3] = "Vegeta";
+        arrayEstatico[4] = "Bob Esponja";
+
+        ArrayList<String> arrayDinamico = new ArrayList<String>();
+    }
+}
