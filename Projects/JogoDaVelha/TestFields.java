5473b7ba545dddf5eb35babcac7e571373d1253a
edvunge <costaedy18@gmail.com>
2024-02-26 02:05:55 +0000

    Day35 - new project_jogoDaVelha

---
+++ Projects/JogoDaVelha/TestFields.java
@@ -0,0 +1,44 @@
+package Projects.JogoDaVelha;
+
+public class TestFields {
+    /*
+    JOGO DA VELHA
+
+
+	|	 |
+	|	 |
+- - - - - - - -
+	|	 |
+	|	 |
+- - - - - - - -
+	|    |
+	|	 |
+
+matriz(3, 3)
+    */
+    public static void main(String[] args) {
+        /*
+        * Metodos necessarias:
+        *
+
+        * adicionarJogada
+
+        * obterJogadasPossiveis
+
+        * verificarVencedor
+
+        * obtemLinhas
+
+        * obtemColunas
+
+        * obtemDiagonais
+
+        * verificarFimDoJogo
+
+        * verificarVencedor
+        * */
+    }
+    /*
+    * https://github.com/b4f-bootcamp-4/code-examples/blob/main/0822/jogo_do_galo.js
+    * */
+}
