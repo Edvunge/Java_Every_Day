a1f89b1a1a50d9f8f1be684f868ea3e49bbc358d
edvunge <costaedy18@gmail.com>
2024-03-02 15:53:23 +0000

    Day40 - exercsmATRIZ

--- LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula20/SomaDeMatrizesBidimenssionais.java
+++ LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula20/SomaDeMatrizesBidimenssionais.java
@@ -1,5 +1,7 @@
 package LoianeJavaBasico.JavaBasico_course_1.General_exercicies.exerc_aula20;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,23 +12,26 @@ public class SomaDeMatrizesBidimenssionais {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
 
-        final int LEN = 0;
-        int linha = 0, coluna;
-        int[][] matM = new int[3][3];
-        int[][] matQ = new int[3][3];
-        int[][] matG = new int[3][3];
+        final int LEN = 3;
+        int linha = 0, coluna = 0;
+        int[][] matM = new int[LEN][LEN];
+        int[][] matQ = new int[LEN][LEN];
+        int[][] matG = new int[LEN][LEN];
 
+        // matriz M 3x3 de forma aleatoria
         for (linha = 0; linha < LEN; linha++) {
             for (coluna = 0; coluna < LEN; coluna++) {
                 matM[linha][coluna] = random.nextInt(10);
             }
         }
 
+        // matriz Q 3x3 de forma aleatoria
         for (linha = 0; linha < LEN; linha++) {
             for (coluna = 0; coluna < LEN; coluna++) {
                 matQ[linha][coluna] = random.nextInt(10);
             }
         }
+
 
         for (linha = 0; linha < LEN; linha++) {
             for (coluna = 0; coluna < LEN; coluna++) {
@@ -34,10 +39,38 @@ public class SomaDeMatrizesBidimenssionais {
             }
         }
 
+
+        // =================================
+        // IMPRESSAO NA TELA DA MATRIZ M
+        // =================================
+        System.out.println("IMPRESSAO NA TELA DA MATRIZ M");
+        for (linha = 0; linha < LEN; linha++) {
+            for (coluna = 0; coluna < LEN; coluna++) {
+                System.out.printf("  %d  ", matM[linha][coluna]);
+            }
+            System.out.println(" \n ");
+        }
+
+        // =================================
+        // IMPRESSAO NA TELA DA MATRIZ Q
+        // =================================
+        System.out.println("IMPRESSAO NA TELA DA MATRIZ Q\n");
+        for (linha = 0; linha < LEN; linha++) {
+            for (coluna = 0; coluna < LEN; coluna++) {
+                System.out.printf("  %d  ", matQ[linha][coluna]);
+            }
+            System.out.println(" \n ");
+        }
+
+        // =================================
+        // IMPRESSAO NA TELA DA MATRIZ G
+        // =================================
+        System.out.println("IMPRESSAO NA TELA DA MATRIZ G");
         for (linha = 0; linha < LEN; linha++) {
             for (coluna = 0; coluna < LEN; coluna++) {
                 System.out.printf("  %d  ", matG[linha][coluna]);
             }
+            System.out.println(" \n ");
         }
         input.close();
     }
---
+++ LoianeJavaBasico/JavaBasico_course_1/General_exercicies/exerc_aula20/TransposicaoDaMatriz.java
@@ -0,0 +1,11 @@
+package LoianeJavaBasico.JavaBasico_course_1.General_exercicies.exerc_aula20;
+
+public class TransposicaoDaMatriz {
+    //  Transposição de Matriz:
+    //  Crie uma matriz 3x4 e crie uma função para calcular e
+    //  imprimir a transposta dessa matriz.
+    public static void main(String[] args) {
+        // 4x3
+
+    }
+}
