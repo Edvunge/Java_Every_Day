a7aaf1c0797e44821dbadb374aa959940bf012fd
edvunge <costaedy18@gmail.com>
2024-03-03 23:47:24 +0000

    Day41 - metodosEstaticos

---
+++ MaratonaJavaViradoNoJiraya_DEVdojo/Parte_2_OOP/E_blocosInicializacao/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package MaratonaJavaViradoNoJiraya_DEVdojo.Parte_2_OOP.E_blocosInicializacao.test;
+
+import MaratonaJavaViradoNoJiraya_DEVdojo.Parte_2_OOP.E_blocosInicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+    }
+}
