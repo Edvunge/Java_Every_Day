6fa18d9cb47be290f1b0356bd0c60a94021b6fc6
edvunge <costaedy18@gmail.com>
2024-03-04 00:50:44 +0000

    Day42 - arraysComObjcts_associacao

---
+++ MaratonaJavaViradoNoJiraya_DEVdojo/Parte_2_OOP/G_associacao/test/JogadorTest01.java
@@ -0,0 +1,13 @@
+package MaratonaJavaViradoNoJiraya_DEVdojo.Parte_2_OOP.G_associacao.test;
+
+import MaratonaJavaViradoNoJiraya_DEVdojo.Parte_2_OOP.G_associacao.dominio.Jogador;
+import MaratonaJavaViradoNoJiraya_DEVdojo.Parte_2_OOP.G_associacao.dominio.Time;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Time time = new Time("Selecao angolana");
+
+
+    }
+}
