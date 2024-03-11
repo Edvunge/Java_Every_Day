022f5a5ab0da17d93c8039c8e285e7b954d67deb
edvunge <costaedy18@gmail.com>
2024-03-11 21:18:28 +0000

    Day49 - exercs_test_intellij

---
+++ ZEDlistaDEexercicios/CadernoDeExercicios/C_EXERCICIOS_ESTRUTURA_CONDICIONAL/HelloWorld.java
@@ -0,0 +1,8 @@
+package ZEDlistaDEexercicios.CadernoDeExercicios.C_EXERCICIOS_ESTRUTURA_CONDICIONAL;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // teste
+        System.out.println("hello world!");
+    }
+}
