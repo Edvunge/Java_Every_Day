fe0e35e3c54492685982db57c1b979b8c7e80ae8
edvunge <costaedy18@gmail.com>
2024-03-14 23:30:12 +0000

    Day52 - JPM_firstSteps

---
+++ JavaProgrammingMasterclass/section_3_FirstSteps/FloatANDdoublePrimitivesTypes.java
@@ -0,0 +1,14 @@
+package JavaProgrammingMasterclass.section_3_FirstSteps;
+
+public class FloatANDdoublePrimitivesTypes {
+    public static void main(String[] args) {
+        float value = 40.3f;
+        double minValue = 49.e3;
+
+        System.out.println(minValue);
+        System.out.println("Float value Range(" + Float.MIN_VALUE + " to "+ Float.MAX_VALUE + ")");
+
+        float myOtherFloatValue = 5.25f;
+        System.out.println(myOtherFloatValue);
+    }
+}
---
+++ JavaProgrammingMasterclass/section_3_FirstSteps/FloatingPointPrecision.java
@@ -0,0 +1,22 @@
+package JavaProgrammingMasterclass.section_3_FirstSteps;
+
+public class FloatingPointPrecision {
+    public static void main(String[] args) {
+        // Floating Point Precision
+
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+
+        System.out.println(myIntValue);
+
+        myDoubleValue = 5d / 2d;
+        System.out.println(myDoubleValue);
+
+        myFloatValue = 5f / 5f;
+        System.out.println(myFloatValue);
+
+        myDoubleValue = 5.00 / 3;
+        System.out.println(myDoubleValue);
+    }
+}
