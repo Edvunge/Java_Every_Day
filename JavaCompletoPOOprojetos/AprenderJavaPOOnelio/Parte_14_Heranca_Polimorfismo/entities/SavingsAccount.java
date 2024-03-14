5f99644af9e076f90c674b2bd40f7fbf7af19803
edvunge <costaedy18@gmail.com>
2024-03-14 00:43:11 +0000

    Day52 - heranca_upcasting_AND_downcasting

---
+++ JavaCompletoPOOprojetos/AprenderJavaPOOnelio/Parte_14_Heranca_Polimorfismo/entities/SavingsAccount.java
@@ -0,0 +1,30 @@
+package JavaCompletoPOOprojetos.AprenderJavaPOOnelio.Parte_14_Heranca_Polimorfismo.entities;
+
+public class SavingsAccount extends Account {
+    private Double interestRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance() {
+        balance += balance * interestRate();
+    }
+
+    private Double interestRate() {
+        return null;
+    }
+}
