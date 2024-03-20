23734f931f9be20b0e5ac6843c19abd1b5bd1dc1
edvunge <costaedy18@gmail.com>
2024-03-20 22:33:03 +0000

    Day58 - aulasPoo and Exercs

---
+++ LoianeJavaBasico/JavaBasicoCourse_1_/aulasPoo/Car.java
@@ -0,0 +1,9 @@
+package LoianeJavaBasico.JavaBasicoCourse_1_.aulasPoo;
+
+public class Car {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
