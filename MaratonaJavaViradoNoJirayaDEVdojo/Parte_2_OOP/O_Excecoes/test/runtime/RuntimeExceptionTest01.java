f439a6e57ee623f612bf19d021a09a940eb05921
edvunge <costaedy18@gmail.com>
2024-03-25 23:24:40 +0000

    Day63 - excepcions

---
+++ MaratonaJavaViradoNoJirayaDEVdojo/Parte_2_OOP/O_Excecoes/test/runtime/RuntimeExceptionTest01.java
@@ -0,0 +1,8 @@
+package MaratonaJavaViradoNoJirayaDEVdojo.Parte_2_OOP.O_Excecoes.test.runtime;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Che
+
+    }
+}
