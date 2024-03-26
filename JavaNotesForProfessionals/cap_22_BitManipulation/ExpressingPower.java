195e9a0b3900417319b1e112a67c121bafa1debe
edvunge <costaedy18@gmail.com>
2024-03-26 19:15:53 +0000

    Day64 - arrays

---
+++ JavaNotesForProfessionals/cap_22_BitManipulation/ExpressingPower.java
@@ -0,0 +1,38 @@
+package JavaNotesForProfessionals.cap_22_BitManipulation;
+
+public class ExpressingPower {
+    public static void main(String[] args) {
+        int twoExp4  = 1<<4;
+        int twoExp5  = 1<<5;
+        int twoExp6  = 1<<6;
+        int twoExp7  = 1<<7;
+        int twoExp8  = 1<<8;
+        int twoExp9  = 1<<9;
+        int twoExp10 = 1<<10;
+        int twoExp11 = 1<<11;
+        int twoExp12 = 1<<12;
+        int twoExp13 = 1<<13;
+        int twoExp14 = 1<<14;
+        int twoExp15 = 1<<15;
+        int twoExp16 = 1<<16;
+        int twoExp17 = 1<<17;
+        int twoExp18 = 1<<18;
+        int twoExp19 = 1<<19;
+        int twoExp20 = 1<<20;
+        int twoExp21 = 1<<21;
+        int twoExp22 = 1<<22;
+        int twoExp23 = 1<<23;
+        int twoExp24 = 1<<24;
+        int twoExp25 = 1<<25;
+        int twoExp26 = 1<<26;
+        int twoExp27 = 1<<27;
+        int twoExp28 = 1<<28;
+        int twoExp29 = 1<<29;
+        int twoExp30 = 1<<30;
+        int twoExp31 = 1<<31;
+
+        //int pow2(int exp) {
+        //    return 1<<exp;
+        //}
+    }
+}
---
+++ JavaNotesForProfessionals/cap_23_Arrays/ListFromArray.java
@@ -0,0 +1,13 @@
+package JavaNotesForProfessionals.cap_23_Arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListFromArray {
+    public static void main(String[] args) {
+        String[] stringArray = { "foo", "bar", "baz"};
+        List<String> stringList = Arrays.asList(stringArray);
+
+
+    }
+}
