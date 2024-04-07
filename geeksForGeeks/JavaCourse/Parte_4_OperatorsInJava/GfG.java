b490493e348afb818891a410991db68e07776d30
edvunge <costaedy18@gmail.com>
2024-04-07 21:24:35 +0100

    Day74 - operators_

---
+++ geeksForGeeks/JavaCourse/Parte_4_OperatorsInJava/GfG.java
@@ -0,0 +1,10 @@
+package geeksForGeeks.JavaCourse.Parte_4_OperatorsInJava;
+
+public class GfG {
+    public static void main(String[] args) {
+        boolean cond = true;
+        int a = 10, b = 1;
+
+        System.out.println();
+    }
+}
---
+++ geeksForGeeks/JavaCourse/Parte_4_OperatorsInJava/Gfg.java
@@ -0,0 +1,13 @@
+package geeksForGeeks.JavaCourse.Parte_4_OperatorsInJava;
+
+public class Gfg {
+    public static void main(String[] args) {
+        int n1 = 20;
+
+        System.out.println("Number = " + n1);
+
+        n1 = -n1;
+
+        System.out.println("Result = " + n1);
+    }
+}
