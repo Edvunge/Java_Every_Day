49092217280664135ec11b3b7158134f2bbe5c66
edvunge <costaedy18@gmail.com>
2024-04-11 00:22:32 +0100

    Day77 - Date_Locale

---
+++ MaratonaJavaViradoNoJirayaDEVdojo/Parte_2_OOP/Rdatas/test/LocaleTest01.java
@@ -0,0 +1,18 @@
+package MaratonaJavaViradoNoJirayaDEVdojo.Parte_2_OOP.Rdatas.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it","IT");
+        Locale localeCH = new Locale("it", "CH");
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df1 =  DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 =  DateFormat.getDateInstance(DateFormat.FULL, localeCH);
+
+        System.out.println("Italia " + df1.format(calendar.getTime()));
+        System.out.println("Suica " + df2.format(calendar.getTime()));
+    }
+}
