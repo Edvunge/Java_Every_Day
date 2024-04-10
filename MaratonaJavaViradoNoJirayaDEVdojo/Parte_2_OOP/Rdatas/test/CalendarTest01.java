00c5bb3d16dedb46267a19fc5d3137ba4bfc4645
edvunge <costaedy18@gmail.com>
2024-04-10 23:58:17 +0100

    Day76 - Dates_and_Calendar

---
+++ MaratonaJavaViradoNoJirayaDEVdojo/Parte_2_OOP/Rdatas/test/CalendarTest01.java
@@ -0,0 +1,19 @@
+package MaratonaJavaViradoNoJirayaDEVdojo.Parte_2_OOP.Rdatas.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest01 {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+
+        if(c.getFirstDayOfWeek() == Calendar.SUNDAY){
+            System.out.println("Domingo e o primeiro dia da semana");
+        }
+
+        c.add(Calendar.DAY_OF_MONTH, 2);
+        c.add(Calendar.HOUR, 10);
+        Date date = c.getTime();
+        System.out.println(date);
+    }
+}
