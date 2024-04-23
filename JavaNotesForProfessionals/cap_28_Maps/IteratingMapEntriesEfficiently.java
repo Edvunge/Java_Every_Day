2a1bc8b231d42860b2accc8b54a8364d5858c8d5
edvunge <costaedy18@gmail.com>
2024-04-23 15:46:21 +0100

    Day89 - Maps

---
+++ JavaNotesForProfessionals/cap_28_Maps/IteratingMapEntriesEfficiently.java
@@ -0,0 +1,11 @@
+package JavaNotesForProfessionals.cap_28_Maps;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class IteratingMapEntriesEfficiently {
+    public static void main(String[] args) {
+        // 1. implementation using Iterator with Map.Entry
+        //Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
+    }
+}
