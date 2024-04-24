055dc59b03ef2d7e9b8e70ee14f5ea93623874dd
edvunge <costaedy18@gmail.com>
2024-04-24 18:08:05 +0100

    day89 - update

---
+++ books/teste.java
@@ -0,0 +1,10 @@
+package books;
+
+import java.util.Locale;
+
+public class teste {
+    public static void main(String[] args) {
+        String name = "edvaldo";
+        System.out.println(name.toLowerCase());
+    }
+}
