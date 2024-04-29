5b7bc8cd281b36594e6d520e8c8b1afd4fc7fd58
edvunge <costaedy18@gmail.com>
2024-04-29 23:56:59 +0100

    Day96 - exercs

--- ExercsResolvidosDeAlgoritmos/Lista_2/part_3/Algoritmo137.java
+++ ExercsResolvidosDeAlgoritmos/Lista_2/part_3/Algoritmo137.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Algoritmo137 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        for (int i = 0; i < 12; i++){
+            System.out.println(i);
+        }
     }
 }
