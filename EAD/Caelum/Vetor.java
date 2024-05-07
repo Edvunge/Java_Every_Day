cba4c148dac89cb1fce5d2e7160cb43e4b3a49cd
edvunge <costaedy18@gmail.com>
2024-05-07 01:56:17 +0100

    Day103 - arrays manipulations II

---
+++ EAD/Caelum/Vetor.java
@@ -0,0 +1,37 @@
+package EAD.Caelum;
+
+import java.util.Arrays;
+
+public class Vetor {
+    // Declarando e Inicializando um array de Aluno com capacidade 100.
+    private Aluno[] alunos = new Aluno[100];
+
+    public void adiciona(Aluno aluno) {
+        // Implementacao
+    }
+
+    public void adiciona(int posicao, Aluno aluno) {
+        // implementacao
+    }
+
+    //public Aluno pega(int posicao) {
+        // implementacao
+    //}
+
+
+    public void remove(int posicao) {
+        // implementacao
+    }
+
+    //public boolean contem(Aluno aluno) {
+        // implementacao
+    //}
+
+    //public int tamanho() {
+        // implementacao
+    //}
+
+    public String toString() {
+        return Arrays.toString(alunos);
+    }
+}
