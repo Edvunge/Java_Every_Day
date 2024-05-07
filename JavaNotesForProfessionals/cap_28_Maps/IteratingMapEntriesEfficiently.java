7ee04c32bae094da1e345bfdf9b8e1dd1421243a
edvunge <costaedy18@gmail.com>
2024-05-07 12:55:02 +0100

    day103 - maps_linkedHashMap and TreeMap

--- JavaNotesForProfessionals/cap_28_Maps/IteratingMapEntriesEfficiently.java
+++ JavaNotesForProfessionals/cap_28_Maps/IteratingMapEntriesEfficiently.java
@@ -7,5 +7,10 @@ public class IteratingMapEntriesEfficiently {
     public static void main(String[] args) {
         // 1. implementation using Iterator with Map.Entry
         //Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
+
+        //2. implementation using Map.forEach(java 8+)
+        //for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
+        //    sum += pair.getKey() + pair.getValue();
+        //}
     }
 }
---
+++ JavaNotesForProfessionals/cap_30_WeakHashMap/WeakHashMapTest.java
@@ -0,0 +1,29 @@
+package JavaNotesForProfessionals.cap_30_WeakHashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class WeakHashMapTest {
+    public static void main(String[] args) {
+        Map hashMap = new HashMap();
+
+        Map weakHashMap = new WeakHashMap();
+
+        String keyHashMap = new String("keyHashMap");
+        String keyWeakHashMap = new String("keyWeakHashMap");
+
+        hashMap.put(keyHashMap, "Ankitta");
+        weakHashMap.put(keyWeakHashMap, "Atul");
+        System.gc();
+        System.out.println("Before: hash map value: " + hashMap.get("keyHashMap") + " and weak hash map value: " + weakHashMap.get("keyWeakHashMap"));
+
+        keyHashMap = null;
+        keyWeakHashMap = null;
+
+        System.gc();
+
+        System.out.println("After: hash map value: " + hashMap.get("keyHashMap") + " and weak hash map value: " + weakHashMap.get("keyWeakHashMap"));
+
+    }
+}
