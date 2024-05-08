abcd90a4081348057d038ff2dc60eb27bd85aa1f
edvunge <costaedy18@gmail.com>
2024-05-08 23:50:46 +0100

    Day104 - oops

---
+++ geeksForGeeks/JavaCourse/Parte_7_OOPS/Student.java
@@ -0,0 +1,12 @@
+package geeksForGeeks.JavaCourse.Parte_7_OOPS;
+
+public class Student {
+    int id;
+    String name;
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
