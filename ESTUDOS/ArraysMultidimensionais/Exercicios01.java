2d00ed99cd244397f75cf1f89b7ec451682ecbef
edvunge <costaedy18@gmail.com>
2024-05-13 19:17:23 +0100

    Day109 - arrays2D_3D

---
+++ ESTUDOS/ArraysMultidimensionais/Exercicios01.java
@@ -0,0 +1,9 @@
+package ESTUDOS.ArraysMultidimensionais;
+
+public class Exercicios01 {
+    // 1. Gere e imprima uma matriz M 4x4 com valores entre 0-9.
+    // Após isso determine o maior número da matriz e a sua posição (linha, coluna).
+    public static void main(String[] args) {
+        int[][] matrizDelta = new int[4][4];
+    }
+}
