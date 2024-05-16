adfac3756a9bf31a60d7ca6b633b11437daff7fd
edvunge <costaedy18@gmail.com>
2024-05-16 14:42:42 +0100

    Day111 - POO_collections

---
+++ JavaNotesForProfessionals/cap_24_Collections/ExampleTestArrays.java
@@ -0,0 +1,11 @@
+package JavaNotesForProfessionals.cap_24_Collections;
+
+public class ExampleTestArrays {
+    public static void main(String[] args) {
+        String[] names = {"James", "Smith", "Sonny", "Huckle", "Berry", "Finn", "Allan", "Edvaldo"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(" " + names[i]);
+        }
+    }
+}
