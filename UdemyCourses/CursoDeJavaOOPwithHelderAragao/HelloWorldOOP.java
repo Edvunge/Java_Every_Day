4cec08aaf47e767bb29b2d147da6c847ff4049e9
edvunge <costaedy18@gmail.com>
2024-05-21 17:02:14 +0100

    Day115 - new_project_udemyCourses

---
+++ UdemyCourses/CursoDeJavaOOPwithHelderAragao/HelloWorldOOP.java
@@ -0,0 +1,14 @@
+package UdemyCourses.CursoDeJavaOOPwithHelderAragao;
+
+public class HelloWorldOOP {
+
+    void greeting() {
+        System.out.println("Hello, World!");
+        System.out.println("In Programing Oriented Object");
+    }
+
+    public static void main(String[] args) {
+        HelloWorldOOP helloWorldOOP = new HelloWorldOOP();
+        helloWorldOOP.greeting();
+    }
+}
