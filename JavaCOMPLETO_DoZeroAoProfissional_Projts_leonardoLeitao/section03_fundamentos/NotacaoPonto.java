b7a583f40159318f7833931c88cdfffb9e93c0cf
edvunge <costaedy18@gmail.com>
2024-05-22 19:03:03 +0100

    Day116 - fundamentos_java

---
+++ JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao/section03_fundamentos/NotacaoPonto.java
@@ -0,0 +1,12 @@
+package JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao.section03_fundamentos;
+
+public class NotacaoPonto {
+    public static void main(String[] args) {
+        double a = 2.3;
+        String s = "Bom Dia X";
+        s = s.toUpperCase();
+        s = s.replace("X", "Senhora");
+
+        System.out.println(s);
+    }
+}
---
+++ JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao/section04_estruturasDeControle/HelloWorld.java
@@ -0,0 +1,9 @@
+package JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao.section04_estruturasDeControle;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Estrutura de controle!");
+        }
+    }
+}
