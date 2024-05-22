9fdee70212323247cafef9ed6e74974fa8406b23
edvunge <costaedy18@gmail.com>
2024-05-22 13:30:05 +0100

    day116 - newPakage_course

---
+++ JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao/section03_fundamentos/HelloWorld.java
@@ -0,0 +1,7 @@
+package JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao.section03_fundamentos;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
