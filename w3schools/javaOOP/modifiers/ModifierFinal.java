cf65f0e8c9c5867ffc9bcd052b5786a80ff8f699
edvunge <costaedy18@gmail.com>
2024-05-29 23:58:32 +0100

    Day121 - modifiers

---
+++ w3schools/javaOOP/modifiers/ModifierFinal.java
@@ -0,0 +1,11 @@
+package w3schools.javaOOP.modifiers;
+
+public class ModifierFinal {
+    final int x = 10;
+    final double PI = 3.14;
+
+    public static void main(String[] args) {
+        ModifierFinal myObj = new ModifierFinal();
+
+    }
+}
