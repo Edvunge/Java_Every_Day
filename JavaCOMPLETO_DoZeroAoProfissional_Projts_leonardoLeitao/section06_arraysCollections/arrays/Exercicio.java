b4d5c70959525d2a26bff5f9bfdbda1bf56a2352
edvunge <costaedy18@gmail.com>
2024-05-29 02:47:48 +0100

    Day121 - arrays_matrizes_forEach

---
+++ JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao/section06_arraysCollections/arrays/Exercicio.java
@@ -0,0 +1,15 @@
+package JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao.section06_arraysCollections.arrays;
+
+import java.util.Arrays;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        double[] notasAlunoA = new double[3];
+        notasAlunoA[0] = 7.9;
+        notasAlunoA[1] = 8.0;
+        notasAlunoA[2] = 6.7;
+
+
+        System.out.println(Arrays.toString(notasAlunoA));
+    }
+}
---
+++ JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao/section06_arraysCollections/arrays/Foreach.java
@@ -0,0 +1,11 @@
+package JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao.section06_arraysCollections.arrays;
+
+public class Foreach {
+    public static void main(String[] args) {
+        double[] notas = { 9.9, 8.7, 7.2, 9.4 };
+
+        for(double nota : notas) {
+            System.out.println(nota);
+        }
+    }
+}
