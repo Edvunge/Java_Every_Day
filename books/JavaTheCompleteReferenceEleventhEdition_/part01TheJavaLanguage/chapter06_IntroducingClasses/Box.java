a86aec6dd2463425453821dac89049443fc0ff15
edvunge <costaedy18@gmail.com>
2024-06-17 22:33:50 +0100

    Day136 - classes

--- books/JavaTheCompleteReferenceEleventhEdition_/part01TheJavaLanguage/chapter06_IntroducingClasses/Box.java
+++ books/JavaTheCompleteReferenceEleventhEdition_/part01TheJavaLanguage/chapter06_IntroducingClasses/Box.java
@@ -4,4 +4,11 @@ public class Box {
     double width;
     double height;
     double depth;
+
+    // display volume of a box
+    double volume() {
+        //System.out.println("Volume is ");
+        //System.out.println(width * height * depth);
+        return width * height * depth;
+    }
 }
---
+++ books/JavaTheCompleteReferenceEleventhEdition_/part01TheJavaLanguage/chapter06_IntroducingClasses/BoxDemo4.java
@@ -0,0 +1,28 @@
+package books.JavaTheCompleteReferenceEleventhEdition_.part01TheJavaLanguage.chapter06_IntroducingClasses;
+
+public class BoxDemo4 {
+    public static void main(String[] args) {
+        Box myBox1 = new Box();
+        Box myBox2 = new Box();
+        double vol;
+
+        // assign values to mybox1's instance variables
+        myBox1.width = 10;
+        myBox1.height = 20;
+        myBox1.depth = 15;
+
+        // assign values to mybox2's instance variables
+        myBox2.width  = 3;
+        myBox2.height = 6;
+        myBox2.depth  = 9;
+
+        // display volume of first box
+        vol = myBox1.volume();
+        System.out.println("volume is " + vol);
+
+        // display volume of second box
+        vol = myBox2.volume();
+        System.out.println("volume is " + vol);
+
+    }
+}
