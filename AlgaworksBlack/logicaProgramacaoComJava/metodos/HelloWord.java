dc0df0c7ea2a24a57c83413250fdd1fe0d322150
edvunge <costaedy18@gmail.com>
2024-07-03 17:12:19 +0100

    Day148 - new_Course

---
+++ AlgaworksBlack/logicaProgramacaoComJava/metodos/HelloWord.java
@@ -0,0 +1,7 @@
+package AlgaworksBlack.logicaProgramacaoComJava.metodos;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
