9b1a2ceb4b4f6825c856137dbdbb4806aa66ba7f
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2024-07-06 23:25:32 +0100

    Update TesteAdicionaNoFim.java

--- EAD/Caelum/TesteAdicionaNoFim.java
+++ EAD/Caelum/TesteAdicionaNoFim.java
@@ -3,15 +3,11 @@ package EAD.Caelum;
 public class TesteAdicionaNoFim {
     public static void main(String[] args) {
         Aluno a1 = new Aluno();
-        Aluno a2 = new Aluno();
-
-        a1.setNome("joao");
-        a2.setNome("jose");
+        
 
         Vetor lista = new Vetor();
 
         lista.adiciona(a1);
-        lista.adiciona(a2);
 
         System.out.println(lista);
     }
