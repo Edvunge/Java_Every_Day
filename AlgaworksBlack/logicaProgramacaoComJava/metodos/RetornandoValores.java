f51cbc8d98f7710bc637aeb642c1cc22799d9647
edvunge <costaedy18@gmail.com>
2024-07-07 01:45:53 +0100

    Day151 - metodosComRetorno_eSemRetorno

---
+++ AlgaworksBlack/logicaProgramacaoComJava/metodos/RetornandoValores.java
@@ -0,0 +1,15 @@
+package AlgaworksBlack.logicaProgramacaoComJava.metodos;
+
+public class RetornandoValores {
+    public static void main(String[] args) {
+        Integer numeroQualquer = numeroAleatorio();
+
+        System.out.println("numero: " + numeroQualquer);
+    }
+
+    static Integer numeroAleatorio() {
+        Integer numero = 1;
+
+        return numero;
+    }
+}
