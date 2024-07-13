8a74d1a9b424cc2ad1a20d3f57a6a2a7025125f2
Edvunge <costaedy18@gmail.com>
2024-07-13 19:17:15 +0100

    Day 158 - poo_

---
+++ AlgaworksBlack/logicaProgramacaoComJava/poo/exercicio03/Pedido.java
@@ -0,0 +1,41 @@
+package AlgaworksBlack.logicaProgramacaoComJava.poo.exercicio03;
+
+public class Pedido {
+
+    double  codigo;
+    double  subtotal;
+    double  desconto;
+
+
+    public double getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(double codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public void getTotal() {
+ 
+    }
+
+    public void setTotal(double total) {
+        total = subtotal - desconto;
+    }
+}
