0e5c962c0bc26556302af6d84a26110aa3857316
Edvunge <costaedy18@gmail.com>
2024-07-14 22:21:16 +0100

    Day 159 - poo_i

--- AlgaworksBlack/logicaProgramacaoComJava/poo/classeInstancia/ClasseInstancia.java
+++ AlgaworksBlack/logicaProgramacaoComJava/poo/classeInstancia/ClasseInstancia.java
@@ -11,11 +11,6 @@ public class ClasseInstancia {
         Produto.QUANTIIDADE_MINIMA_ESTOQUE = 11;
         System.out.println("Quantidade Minima Estoque: " + Produto.QUANTIIDADE_MINIMA_ESTOQUE);
 
-
-        Produto produto01 = new Produto();
-        produto01.nome = "carteira";
-
         System.out.println("Produto 0: " + produto00.nome);
-        System.out.println("Produto 1: " + produto01.nome);
     }
 }
