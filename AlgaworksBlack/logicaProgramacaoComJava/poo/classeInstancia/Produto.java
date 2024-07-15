7ddfb8d7378604d25cd58028e4b56580b538e615
Edvunge <costaedy18@gmail.com>
2024-07-15 23:04:38 +0100

    Day 160 - poo

--- AlgaworksBlack/logicaProgramacaoComJava/poo/classeInstancia/Produto.java
+++ AlgaworksBlack/logicaProgramacaoComJava/poo/classeInstancia/Produto.java
@@ -3,4 +3,5 @@ package AlgaworksBlack.logicaProgramacaoComJava.poo.classeInstancia;
 public class Produto {
     static Integer QUANTIIDADE_MINIMA_ESTOQUE = 1;
     String nome;
+    double tamanho;
 }
