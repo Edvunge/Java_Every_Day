cbde531ca9021d74d065168667de332cb58520d3
edvaldo <costaedy18@gmail.com>
2024-07-27 00:49:58 +0100

    update_new_machine

--- Tests/HelloWorld.java
+++ Tests/HelloWorld.java
@@ -7,7 +7,7 @@
     public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello , World!");
-            System.out.println(ªª)
+            System.out.println("Hello test winds 10 pro");
         }
     }
 
