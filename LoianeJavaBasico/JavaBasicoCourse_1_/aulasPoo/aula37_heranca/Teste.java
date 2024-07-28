e90152d435e3c0d50d7d14d7de9bbaaae25e9613
edvaldo <costaedy18@gmail.com>
2024-07-29 00:30:47 +0100

    heranca_palavraChaveSuper

---
+++ LoianeJavaBasico/JavaBasicoCourse_1_/aulasPoo/aula37_heranca/Teste.java
@@ -0,0 +1,13 @@
+package LoianeJavaBasico.JavaBasicoCourse_1_.aulasPoo.aula37_heranca;
+
+public class Teste {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("maria");
+    }
+}
