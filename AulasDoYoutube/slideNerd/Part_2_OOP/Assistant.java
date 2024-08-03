39236c957eb97268aa56d572ce965034602414d5
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2024-08-03 23:12:35 +0100

    Update Assistant.java

--- AulasDoYoutube/slideNerd/Part_2_OOP/Assistant.java
+++ AulasDoYoutube/slideNerd/Part_2_OOP/Assistant.java
@@ -2,7 +2,7 @@ package AulasDoYoutube.slideNerd.Part_2_OOP;
 
 public class Assistant {
     void add(int a, int b) {
-        int sum = a + b;
+        int sum = a + b + 89;
         System.out.println(sum+2);
     }
 }
