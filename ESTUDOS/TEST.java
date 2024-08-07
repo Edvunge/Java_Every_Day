483588754d906c9151d5d5bdd7022ac207a90d56
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2024-08-07 23:30:55 +0100

    Update TEST.java

--- ESTUDOS/TEST.java
+++ ESTUDOS/TEST.java
@@ -7,7 +7,7 @@ public class TEST {
             for (int j = 0; j < 10; j++) {
                 if (i == 5 && j ==5) {
                     break outer;
-                    // breaks out of outer loop instead of only the inner one
+                    
                 }
             }
         }
