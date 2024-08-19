ffde13e75f7f0b912f442d4e4102944e586e6dd8
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2024-08-19 23:53:09 +0100

    Update BirthdayText.java

--- ESTUDOS/BirthdayText.java
+++ ESTUDOS/BirthdayText.java
@@ -3,6 +3,11 @@ package ESTUDOS;
 
 public class BirthdayText {
     public static void main(String[] args) {
-        System.out.println("Today Is My-  Fucking Birthday!!");
+        System.out.println("1. Today Is My -  Fucking Birthday!!");
+        System.out.println("2. Today Is My -  Fucking Birthday!!");
+        System.out.println("3. Today Is My -  Fucking Birthday!!");
+        System.out.println("4. Today Is My -  Fucking Birthday!!");
+        System.out.println("5. Today Is My -  Fucking Birthday!!");
+        System.out.println("6. Today Is My -  Fucking Birthday!!");
     }
 }
