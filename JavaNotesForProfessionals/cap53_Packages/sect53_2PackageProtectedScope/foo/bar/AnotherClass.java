50a2dfe986db35c0ef2eebf36d15f0f8108b49b7
edvaldo <costaedy18@gmail.com>
2024-08-24 22:07:00 +0100

    update

--- JavaNotesForProfessionals/cap53_Packages/sect53_2PackageProtectedScope/foo/bar/AnotherClass.java
+++ JavaNotesForProfessionals/cap53_Packages/sect53_2PackageProtectedScope/foo/bar/AnotherClass.java
@@ -1,4 +1,9 @@
 package JavaNotesForProfessionals.cap53_Packages.sect53_2PackageProtectedScope.foo.bar;
 
 public class AnotherClass {
+    public static void main(String[] args) {
+        System.out.println("....");
+        System.out.println("eeee");
+        System.out.println("....");
+    }
 }
