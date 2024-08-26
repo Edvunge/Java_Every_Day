2748ac5a14b6992fcdc9c68e74646ee32a12d01a
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2024-08-26 23:26:51 +0100

    Update HelloWorldSection1.java

--- CompleteIntensiveJava_AppliedToPractice/section_1_fundamentos/HelloWorldSection1.java
+++ CompleteIntensiveJava_AppliedToPractice/section_1_fundamentos/HelloWorldSection1.java
@@ -3,6 +3,6 @@ package CompleteIntensiveJava_AppliedToPractice.section_1_fundamentos;
 public class HelloWorldSection1 {
     public static void main(String[] args) {
         System.out.println("Hell World!...");
-        System.out.println("Section 1: Fundamentos");
+        System.out.println("Section 1: Fundamentos...");
     }
 }
