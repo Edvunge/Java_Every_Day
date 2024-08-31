947f4bdab8b8850a25048d2d6b342a26c3d57eb9
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2024-08-31 23:33:09 +0100

    Update Boss.java

--- AulasDoYoutube/slideNerd/Part_2_OOP/Boss.java
+++ AulasDoYoutube/slideNerd/Part_2_OOP/Boss.java
@@ -4,5 +4,6 @@ public class Boss {
     public static void main(String[] args) {
         Assistant assistant = new Assistant();
         assistant.add(33, 35);
+        assistant.add(33, 35);
     }
 }
