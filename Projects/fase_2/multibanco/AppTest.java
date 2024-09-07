db6c43d25ab36e7e654781eed4a8e05018e6647e
edvaldo <costaedy18@gmail.com>
2024-09-07 15:10:57 +0100

    new project

---
+++ Projects/fase_2/multibanco/AppTest.java
@@ -0,0 +1,33 @@
+package Projects.fase_2.multibanco;
+
+import java.util.Scanner;
+
+public class AppTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pin = 1234;
+
+        /*
+
+        quero que o programa peca uma senha e que verifique
+        caso essa senha seje igual a um determinado pin,  
+
+
+        */
+        //do {
+            for (int i = 1; i < 4; i++) {
+
+                System.out.printf("%d - tentativa: Digite o seu pin: ", i);
+                int x = input.nextInt();
+
+                if (pin == 1234) {
+                    System.out.println("!!!! - PIN CORRETO - !!!!");
+                    break;
+                }
+                System.out.println("Rodou: ");
+            }
+       // } while ();
+        System.out.println(" FIM!!! ");
+
+    }
+}
---
+++ Projects/fase_2/multibanco/Cartao.java
@@ -0,0 +1,30 @@
+package Projects.fase_2.multibanco;
+
+public class Cartao {
+    private int pin;
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    void validarPin(int pin) {
+
+
+
+        //if (pin == 1234) {
+
+        //}
+    }
+    // pin
+    // validar pin
+    // verificar validade do cartao
+    //          caso nao esteje valido
+    //                  reter cartao
+    //          caso esteje valido
+    //                  avancar para outras operacoes
+
+}
