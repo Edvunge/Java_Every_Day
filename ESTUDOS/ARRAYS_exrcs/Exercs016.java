64d2f6947aee6ae2aaca23d58cab5d757bb52970
Edvunge <costaedy18@gmail.com>
2024-09-11 21:59:28 +0100

    Day 168 - update

--- ESTUDOS/ARRAYS_exrcs/Exercs016.java
+++ ESTUDOS/ARRAYS_exrcs/Exercs016.java
@@ -14,17 +14,17 @@ public class Exercs016 {
     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        final int LENM = 100;
+        final int LENM = 10;
         int[] numbers = new int[LENM];
-        int numersIguaisA30 = 0, soma = 0, numerosMaioresQueAmedia = 0;
+        int numersIguaisA30 = 0, sum = 0, numerosMaioresQueAmedia = 0;
         int numersIguaisAmedia = 0;
-        double media = 0;
+        double median = 0;
 
 
         for (int n = 0; n < LENM; n++) {
             System.out.printf("Digite o %d element: ",n);
             numbers[n] = input.nextInt();
-            soma += numbers[n];
+            sum += numbers[n];
         }
 
 
@@ -34,16 +34,16 @@ public class Exercs016 {
                 numersIguaisA30++;
             }
 
-            if (numbers[line] > media) {
+            if (numbers[line] > median) {
                 numerosMaioresQueAmedia++;
             }
 
-            if (numbers[line] == media){
+            if (numbers[line] == median){
                 numersIguaisAmedia++;
             }
         }
 
-        media = (soma/LENM);
+        median = (sum/LENM);
         System.out.println(" ");
         System.out.println("Estes numeros sao iguais a 30: " + numersIguaisA30);
         System.out.println("Estes numeros sao maiores que a media: " + numerosMaioresQueAmedia);
