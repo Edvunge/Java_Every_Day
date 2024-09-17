50bb95b74c94e5b1f7e85c09ca026c4828b87eee
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2024-09-17 22:50:44 +0100

    Update Teste.java

--- LoianeJavaBasico/JavaBasicoCourse_1_/aulasPoo/aula45CastingHeranca/Teste.java
+++ LoianeJavaBasico/JavaBasicoCourse_1_/aulasPoo/aula45CastingHeranca/Teste.java
@@ -1,6 +1,5 @@
 package LoianeJavaBasico.JavaBasicoCourse_1_.aulasPoo.aula45CastingHeranca;
 
 public class Teste {
-    //Papagaio papagaio = new Papagaio();
-    //Animal animal = papagaio; // casting
+ 
 }
