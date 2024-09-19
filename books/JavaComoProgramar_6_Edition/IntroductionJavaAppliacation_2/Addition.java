9bc98c987f405a8d48f2a456657d3895eee64105
edvaldo <costaedy18@gmail.com>
2024-09-19 23:14:20 +0100

    new course

---
+++ books/JavaComoProgramar_6_Edition/IntroductionJavaAppliacation_2/Addition.java
@@ -0,0 +1,22 @@
+package books.JavaComoProgramar_6_Edition.IntroductionJavaAppliacation_2;
+
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1;
+        int number2;
+        int sum;
+
+        System.out.println("Enter first integer:  ");
+        number1 = input.nextInt();
+
+        System.out.println("Enter second integer:  ");
+        number2 = input.nextInt();
+
+        sum = number1 + number2;
+
+        System.out.printf("Sum is %d\n", sum);
+    }
+}
---
+++ books/JavaComoProgramar_6_Edition/IntroductionJavaAppliacation_2/SomandoInteiros.java
@@ -0,0 +1,23 @@
+package books.JavaComoProgramar_6_Edition.IntroductionJavaAppliacation_2;
+
+import java.util.Scanner;
+
+public class SomandoInteiros {
+    Scanner input = new Scanner(System.in);
+    int c, thisIsAVariable, q76354, number;
+    // a) Declare que as variáveis c, thisIsAVariable, q76354 e number serão do tipo int.
+
+
+    // b) Solicite que o usuário insira um inteiro.
+
+    // c) Insira um inteiro e atribua o resultado à variável int value. Suponha que variável Scanner input possa ser utilizada para ler um valor digitado pelo usuário.
+
+    // d) Imprima "This is a Java program" em uma linha na janela de comando. Utilize o método de uso System.out.println.
+
+    // e) Imprima "This is a Java program" em duas linhas na janela de comando. A primeira linha deve terminar com Java. Utilize o método System.out.println.
+
+    // f) Imprima "This is a Java program" em duas linhas na janela de comando. A primeira linha deve terminar com Java. Utilize o método System.out.printf e dois especificadores de formato %s.
+
+    // g) Se a variável number não for igual a 7, exiba "The variable number is not equal to 7".
+
+}
