0266f795c162bd6067111754526f13590d238e3e
edvaldo <costaedy18@gmail.com>
2024-09-22 20:51:09 +0100

    update

--- books/ProgramacaoEmJavaCursoCompletoPedroCoelho/Parte_I_IntroducaoAoJava/HelloWorld.java
+++ books/ProgramacaoEmJavaCursoCompletoPedroCoelho/Parte_I_IntroducaoAoJava/HelloWorld.java
@@ -2,6 +2,6 @@ package books.ProgramacaoEmJavaCursoCompletoPedroCoelho.Parte_I_IntroducaoAoJava
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!!");
     }
 }
