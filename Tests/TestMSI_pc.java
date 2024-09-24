a9ef9293804b44830a0704effeaf4e74344c51d5
Edvaldo vunge <costaedy18@gmail.com>
2024-09-24 22:43:10 +0100

    test_update

--- Tests/TestMSI_pc.java
+++ Tests/TestMSI_pc.java
@@ -3,6 +3,5 @@ package Tests;
 public class TestMSI_pc {
     public static void main(String[] args) {
         System.out.println("Hello World! MSI - PC");
-        System.out.println("another test");
     }
 }
