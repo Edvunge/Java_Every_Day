a8cfb13cf8f3b44e829d6f3f02f87b47a21deefd
edvaldo <costaedy18@gmail.com>
2024-09-29 23:34:55 +0100

    update

--- LoianeJavaBasico/JavaMediumCourse_2_/Enumeradores_01/Hello.java
+++ LoianeJavaBasico/JavaMediumCourse_2_/Enumeradores_01/Hello.java
@@ -3,5 +3,8 @@ package LoianeJavaBasico.JavaMediumCourse_2_.Enumeradores_01;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
