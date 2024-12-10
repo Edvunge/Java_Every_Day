164a445d66946c35ae47cc54faf762fe8e314e86
edvaldovunge <costaedy18@gmail.com>
2024-12-10 23:52:07 +0000

    newPackage

---
+++ AulasDoYoutube/moddingKaupenjoe/javaIntrodutionForMinecraft/DataType.java
@@ -0,0 +1,24 @@
+package AulasDoYoutube.moddingKaupenjoe.javaIntrodutionForMinecraft;
+
+public class DataType {
+    public static void main(String[] args) {
+        // DATA TYPE AND VARIABLES
+
+        // integers
+        int points;
+        points = 100;
+        int x = -69;
+
+        // floating
+        float pi = 3.14151614f;
+        double e = 2.718763674;
+
+        // Booleans
+        boolean likedVideos = true;
+        boolean subscribed = false;
+
+        // Chars & Strings
+        char grade = 'B';
+        String name = "edvaldo";
+    }
+}
---
+++ AulasDoYoutube/moddingKaupenjoe/javaIntrodutionForMinecraft/Syntax.java
@@ -0,0 +1,11 @@
+package AulasDoYoutube.moddingKaupenjoe.javaIntrodutionForMinecraft;
+
+public class Syntax {
+    public static void main(String[] args) {
+        int points;
+        points = 100;
+        int x = -69;
+
+
+    }
+}
