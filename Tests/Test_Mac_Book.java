d6ae39a57a49047fdff3bb7532c4cd7cb47991bb
Edvaldo Vunge <edvaldovunge@Air-de-Edvaldo.lan>
2024-12-10 18:34:22 +0000

    test_00_mac

---
+++ Tests/Test_Mac_Book.java
@@ -0,0 +1,8 @@
+package Tests;
+
+public class Test_Mac_Book {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("this is a test of mac computer.");
+    }
+}
