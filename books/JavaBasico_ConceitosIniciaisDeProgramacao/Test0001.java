84fb4082d8c99a1e306d0abacf6da367d89b0be7
Edvunge <costaedy18@gmail.com>
2024-12-24 22:41:15 +0000

    refactor

--- books/JavaBasico_ConceitosIniciaisDeProgramacao/Test0001.java
+++ books/JavaBasico_ConceitosIniciaisDeProgramacao/Test0001.java
@@ -1,7 +1,7 @@
 package books.JavaBasico_ConceitosIniciaisDeProgramacao;
 
-public class Test002025 {
+public class Test0001 {
     public static void main(String[] args) {
-        System.out.println("teste0001");
+        System.out.println("teste0002");
     }
 }
