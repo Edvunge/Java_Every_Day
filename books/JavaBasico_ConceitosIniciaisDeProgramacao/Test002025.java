295a4a9eb6f56dd8af88ff3a44d4354af8a3229c
edvaldovunge <costaedy18@gmail.com>
2024-12-24 22:35:21 +0000

    teste0001

---
+++ books/JavaBasico_ConceitosIniciaisDeProgramacao/Test002025.java
@@ -0,0 +1,7 @@
+package books.JavaBasico_ConceitosIniciaisDeProgramacao;
+
+public class Test002025 {
+    public static void main(String[] args) {
+        System.out.println("teste0001");
+    }
+}
