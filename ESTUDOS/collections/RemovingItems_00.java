011d51245a486bc9f64132dab56a21af1b1fd8f3
edvaldovunge <costaedy18@gmail.com>
2024-12-27 01:25:51 +0000

    removing itens

--- ESTUDOS/collections/RemovingItems_00.java
+++ ESTUDOS/collections/RemovingItems_00.java
@@ -1,6 +1,7 @@
 package ESTUDOS.collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class RemovingItems_00 {
@@ -11,5 +12,35 @@ public class RemovingItems_00 {
         fruits.add("Strawberry");
 
         System.out.println("lista de frutas: " + fruits);
+
+        //System.out.println("Depois de removido: "+fruits.get(0));
+
+        //fruits.remove("Apple");
+        //System.out.println("lista de frutas: " + fruits);
+
+        // Uma outra forma de remover um item em uma lista
+        // Removing in while loop using an Iterator
+        /*Iterator<String> fruitIterator = fruits.iterator();
+        while (fruitIterator.hasNext()) {
+            String fruit = fruitIterator.next();
+            System.out.println(fruit);
+
+            if ("Apple".equals(fruit)) {
+                fruitIterator.remove();
+            }
+        }*/
+
+        // Iterating backwards
+        /*for (int i = (fruits.size() - 1); i >= 0; i--) {
+            System.out.println(fruits.get(i));
+            if ("Apple".equals(fruits.get(i))) {
+                fruits.remove(i);
+            }
+        }*/
+
+
+        System.out.println(" --- Removing --- ");
+        System.out.println("lista de frutas: " + fruits);
+
     }
 }
