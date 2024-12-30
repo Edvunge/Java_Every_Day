e92267a4d3b6c303d9bc5ddf4462155bf806426c
edvaldovunge <costaedy18@gmail.com>
2024-12-30 23:06:55 +0000

    update_exercises

--- sites/BeeCrowd/section01_iniciante/Exe1014_Consumo.java
+++ sites/BeeCrowd/section01_iniciante/Exe1014_Consumo.java
@@ -1,4 +1,14 @@
 package sites.BeeCrowd.section01_iniciante;
 
+import java.util.Scanner;
+
 public class Exe1014_Consumo {
+    /*
+        Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km)
+        e o total de combustível gasto (em litros).
+    */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
 }
