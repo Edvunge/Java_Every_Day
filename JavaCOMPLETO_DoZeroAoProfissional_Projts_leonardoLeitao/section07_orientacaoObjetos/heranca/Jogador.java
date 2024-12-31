3f974408140ab8e9a116d51af06f6f182d07c2e2
edvaldovunge <costaedy18@gmail.com>
2024-12-31 10:42:00 +0000

    poo_heranca

---
+++ JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao/section07_orientacaoObjetos/heranca/Jogador.java
@@ -0,0 +1,41 @@
+package JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao.section07_orientacaoObjetos.heranca;
+
+public class Jogador {
+    int x;
+    int y;
+    int vida = 100;
+
+    boolean atacar(Jogador oponente) {
+        int deltaX = Math.abs(x - oponente.x);
+        int deltaY = Math.abs(y - oponente.y);
+
+        if(deltaX == 0 && deltaY == 1) {
+            oponente.vida -= 10;
+            return true;
+        } else if (deltaX == 1 && deltaY == 0) {
+            oponente.vida -= 10;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean andar(Direccao direccao) {
+        switch (direccao) {
+            case NORTE:
+                y--;
+                break;
+            case LESTE:
+                x++;
+                break;
+            case SUL:
+                y++;
+                break;
+            case OESTE:
+                x--;
+                break;
+        }
+
+        return true;
+    }
+}
---
+++ JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao/section07_orientacaoObjetos/heranca/Monstro.java
@@ -0,0 +1,4 @@
+package JavaCOMPLETO_DoZeroAoProfissional_Projts_leonardoLeitao.section07_orientacaoObjetos.heranca;
+
+public class Monstro extends Heroi {
+}
