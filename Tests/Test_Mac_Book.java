a5ae7071d7740315f2fb624ff0f546d1804efa95
edvaldovunge <costaedy18@gmail.com>
2025-01-14 00:42:39 +0000

    test01

--- Tests/Test_Mac_Book.java
+++ Tests/Test_Mac_Book.java
@@ -3,7 +3,10 @@ package Tests;
 public class Test_Mac_Book {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
         System.out.println("this is a second test");
-        System.out.println("this is a thrid test");
+        System.out.println("this is a second test");
+        System.out.println("this is a second test");
     }
 }
