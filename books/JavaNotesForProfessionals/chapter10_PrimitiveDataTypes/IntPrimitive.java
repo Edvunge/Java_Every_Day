acd697a9d3ddaff8b507e9096613c598d6e0c680
edvaldovunge <costaedy18@gmail.com>
2025-01-17 23:51:43 +0000

    update

---
+++ books/JavaNotesForProfessionals/chapter10_PrimitiveDataTypes/IntPrimitive.java
@@ -0,0 +1,7 @@
+package books.JavaNotesForProfessionals.chapter10_PrimitiveDataTypes;
+
+public class IntPrimitive {
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+}
