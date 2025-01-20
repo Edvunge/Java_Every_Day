5e5e52f90b92a555f14573fb8ba6c3d47b2c6f3b
edvaldovunge <costaedy18@gmail.com>
2025-01-20 14:25:12 +0000

    chapter5_operatorsPart1

---
+++ books/Thinking_in_java/chapter5_operators/EqualsMethod2.java
@@ -0,0 +1,14 @@
+package books.Thinking_in_java.chapter5_operators;
+
+class Value {
+    int i;
+}
+
+public class EqualsMethod2 {
+    public static void main(String[] args) {
+        Value v1 = new Value();
+        Value v2 = new Value();
+        v1.i = v2.i = 100;
+        System.out.println(v1.equals(v2));
+    }
+}
