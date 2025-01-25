ffe2270ebf003485041b5ae730f2c001cfab646c
edvaldovunge <costaedy18@gmail.com>
2025-01-25 16:03:06 +0000

    FizzBuzzAlgorithm

--- books/AlgorithmsNotesForProfessionals/chapter01_GettingStarted/FizzBuzzAlgorithm.java
+++ books/AlgorithmsNotesForProfessionals/chapter01_GettingStarted/FizzBuzzAlgorithm.java
@@ -2,6 +2,18 @@ package books.AlgorithmsNotesForProfessionals.chapter01_GettingStarted;
 
 public class FizzBuzzAlgorithm {
     public static void main(String[] args) {
+        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
+        for (int num = 0; num <= number.length; num++) {
+            if (num % 3 == 0 && num % 5 == 0) {
+                System.out.println("(num) fizz buzz");
+            } else if (num % 3 == 0){
+                System.out.println("(num) fizz");
+            } else if (num % 5 == 0) {
+                System.out.println("(num) buzz");
+            } else {
+                System.out.println(num);
+            }
+        }
     }
 }
