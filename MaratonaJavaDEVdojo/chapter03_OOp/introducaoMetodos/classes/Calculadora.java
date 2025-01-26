c0613dee692c0015880bf34070bfa092349d4dbf
edvaldovunge <costaedy18@gmail.com>
2025-01-26 20:51:46 +0000

    parametros

--- MaratonaJavaDEVdojo/chapter03_OOp/introducaoMetodos/classes/Calculadora.java
+++ MaratonaJavaDEVdojo/chapter03_OOp/introducaoMetodos/classes/Calculadora.java
@@ -21,11 +21,26 @@ public class Calculadora {
         return 0;
     }
 
+    /*
+    public double imprimeDoisNumerosDivididos(double num1, double num2) {
+        if(num2 != 0) {
+            return num1 / num2;
+        } else {
+            return 0;
+        }
+    }
+    */
+
     public void imprimeDoisNumerosDivididos(double num1, double num2) {
-        System.out.println(num1/num2);
         if (num2 != 0) {
             System.out.println(num1 / num2);
+            return;
         }
-        System.out.println(num1 / num2);
+        System.out.println("Não é possivel dividir por 0");
+    }
+
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 30;
+        num2 = 40;
     }
 }
--- MaratonaJavaDEVdojo/chapter03_OOp/introducaoMetodos/tests/CalculadoraTest.java
+++ MaratonaJavaDEVdojo/chapter03_OOp/introducaoMetodos/tests/CalculadoraTest.java
@@ -4,10 +4,15 @@ import MaratonaJavaDEVdojo.chapter03_OOp.introducaoMetodos.classes.Calculadora;
 
 public class CalculadoraTest {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
-        calculadora.somaDoisNumeros();
-        calculadora.subtraiDoisNumeros();
-        calculadora.multiplicaDoisNumeros(6, 2);
-        calculadora.divideDoisNumeros(20, 5);
+        Calculadora calc = new Calculadora();
+        calc.somaDoisNumeros();
+        calc.subtraiDoisNumeros();
+        calc.multiplicaDoisNumeros(6, 2);
+        System.out.println("divisao de dois numeros");
+        double result = calc.divideDoisNumeros(20, 0);
+        System.out.println(result);
+        System.out.println("imprimindo dois numeros divididos");
+        calc.imprimeDoisNumerosDivididos(20,5);
+        System.out.println("Continuando exec");
     }
 }
