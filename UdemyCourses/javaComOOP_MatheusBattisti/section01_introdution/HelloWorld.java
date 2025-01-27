67aa825f4c025aa7046ee043db211059c170891a
edvaldovunge <costaedy18@gmail.com>
2025-01-27 16:09:52 +0000

    startNewCourse

---
+++ UdemyCourses/javaComOOP_MatheusBattisti/section01_introdution/HelloWorld.java
@@ -0,0 +1,7 @@
+package UdemyCourses.javaComOOP_MatheusBattisti.section01_introdution;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("start new course!");
+    }
+}
