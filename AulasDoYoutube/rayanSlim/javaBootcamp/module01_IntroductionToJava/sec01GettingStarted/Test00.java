ab3b3f4271c3c65cd731b42e973801cd5f1aeb31
Edvunge <costaedy18@gmail.com>
2025-01-29 16:26:56 +0000

    all_update

---
+++ AulasDoYoutube/rayanSlim/javaBootcamp/module01_IntroductionToJava/sec01GettingStarted/Test00.java
@@ -0,0 +1,7 @@
+package AulasDoYoutube.rayanSlim.javaBootcamp.module01_IntroductionToJava.sec01GettingStarted;
+
+public class Test00 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
