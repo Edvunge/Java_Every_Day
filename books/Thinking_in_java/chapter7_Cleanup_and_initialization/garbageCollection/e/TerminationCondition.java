721ad532dc2dec1db597e9bac972dae963cec292
edvaldovunge <costaedy18@gmail.com>
2025-02-22 14:59:35 +0000

    new course - data structure and springBoot

--- books/Thinking_in_java/chapter7_Cleanup_and_initialization/garbageCollection/e/TerminationCondition.java
+++ books/Thinking_in_java/chapter7_Cleanup_and_initialization/garbageCollection/e/TerminationCondition.java
@@ -1,4 +1,27 @@
 package books.Thinking_in_java.chapter7_Cleanup_and_initialization.garbageCollection.e;
 
+class Book {
+    boolean checkedOut = false;
+    Book(boolean checkOut) {
+        checkedOut = checkOut;
+    }
+    void checkIn() {
+        checkedOut = false;
+    }
+    protected void finalize() {
+        if(checkedOut)
+            System.out.println("Error: checked out");
+    }
+}
+
 public class TerminationCondition {
+    public static void main(String[] args) {
+        Book novel = new Book(true);
+        // Proper cleanup:
+        novel.checkIn();
+        // Drop the reference, forget to clean up:
+        new Book(true);
+        // // Force garbage collection & finalization:
+        System.gc();
+    }
 }
---
+++ courseraORG/dataStructures_and_BackendWithJava/module01_dataStructures/HelloWorld_.java
@@ -0,0 +1,9 @@
+package courseraORG.dataStructures_and_BackendWithJava.module01_dataStructures;
+
+public class HelloWorld_ {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Data structures in java");
+        System.out.println("In coursera");
+    }
+}
