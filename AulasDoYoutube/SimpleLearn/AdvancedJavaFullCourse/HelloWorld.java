ee9e729b73960f2134f8f21e98350bcf921a41f9
edvaldovunge <costaedy18@gmail.com>
2025-02-25 01:51:52 +0000

    Advanced Java Full Course!

---
+++ AulasDoYoutube/SimpleLearn/AdvancedJavaFullCourse/HelloWorld.java
@@ -0,0 +1,7 @@
+package AulasDoYoutube.SimpleLearn.AdvancedJavaFullCourse;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Advanced Java Full Course!");
+    }
+}
