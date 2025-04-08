00e25c7509172a6e34968357069d0f4590a19eba
edvaldovunge <costaedy18@gmail.com>
2025-04-08 01:48:44 +0100

    starting - stream API

---
+++ JavaComplT_Profissional_leonardoLeitao/section09_StreamAPI/ImprimindoObjetos.java
@@ -0,0 +1,27 @@
+package JavaComplT_Profissional_leonardoLeitao.section09_StreamAPI;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ImprimindoObjetos {
+    public static void main(String[] args) {
+        List<String> aprovados = Arrays.asList("Lu", "Gui","Luca","Ana");
+
+        System.out.println("Usando o Foreach...");
+        for (String nome : aprovados) {
+            System.out.println(nome);
+        }
+
+        System.out.println("\nUsando Iterator...");
+        Iterator<String> it = aprovados.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println("\nUsando Stream...");
+        Stream<String> stream = aprovados.stream();
+        stream.forEach(System.out::println);
+    }
+}
