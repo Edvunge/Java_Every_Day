a29df4c0919fe51172b5f105f3827bbdb72309f0
edvaldovunge <costaedy18@gmail.com>
2025-05-09 00:55:09 +0100

    new package

---
+++ AulasDoYoutube/ProgrammingWithMosh/JavaCollectionsTutorial/Main.java
@@ -0,0 +1,12 @@
+package AulasDoYoutube.ProgrammingWithMosh.JavaCollectionsTutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new GenericList<String>();
+        list.add("a");
+        list.add("b");
+
+        for (var item : list.items)
+            System.out.println(item);
+    }
+}
