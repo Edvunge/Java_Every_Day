9f9e1242bf09b6d426d631d8cb558e246a1d3b62
edvaldovunge <costaedy18@gmail.com>
2025-05-12 13:08:10 +0100

    project mad libs game

---
+++ AulasDoYoutube/broCode/oop/java_Full_Course_2025/chap05_arithmetic/Main.java
@@ -0,0 +1,26 @@
+package AulasDoYoutube.broCode.oop.java_Full_Course_2025.chap05_arithmetic;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // Augmented Assignment Operators
+        z = x + y; // Soma
+        z = x - y; // Subtracção
+        z = x * y; // Multiplicação
+        z = x / y; // Divisão
+
+        System.out.println(z);
+
+        // Increment and Decrement Operators
+
+        int x1 = 1;
+
+        x1 += 1;
+
+        System.out.println(x1);
+
+    }
+}
