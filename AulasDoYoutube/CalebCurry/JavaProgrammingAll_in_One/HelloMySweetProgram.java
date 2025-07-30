01e6eba007e967a0043af8ba050a9e55c1bcefbc
edvaldovunge <costaedy18@gmail.com>
2025-07-30 22:38:47 +0100

    update 2

---
+++ AulasDoYoutube/CalebCurry/JavaProgrammingAll_in_One/HelloMySweetProgram.java
@@ -0,0 +1,7 @@
+package AulasDoYoutube.CalebCurry.JavaProgrammingAll_in_One;
+
+public class HelloMySweetProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, There!");
+    }
+}
